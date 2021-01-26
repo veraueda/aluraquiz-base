953b1bd3a10c39b5e32d9936082be3f6ea55813d
Vera Ueda <veraueda@hotmail.com>
2021-01-26 01:23:01 -0300

    1a parte React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+//const Title = styled.h1`
+//  font-size: 50px;
+//  color: ${({ theme }) => theme.colors.primary};
+//`
+
+//function Title(props) { //propcidades / propriedades <3
+//   return (
+//    <h1>
+//      {props.children}
+//    </h1>
+//    )
+//}
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+   <BackgroundImage>
+      ashuashuahshua
+   </BackgroundImage>
+  ); 
+//  <Title>My page</Title>
 }
