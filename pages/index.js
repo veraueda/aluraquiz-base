ab34f7a8825a8baaf162aa7cfcf9922d5b0cf14b
Vera Ueda <veraueda@hotmail.com>
2021-01-27 17:10:17 -0300

    Codigo ao final da primeira aula da imersao React

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,54 @@
 import styled from 'styled-components'
 import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-//const Title = styled.h1`
-//  font-size: 50px;
-//  color: ${({ theme }) => theme.colors.primary};
-//`
+//const BackgroundImage = styled.div`
+//  background-image: url(${db.bg});
+//  flex: 1;
+//  background-size: cover;
+//  background-position: center;
+//`;
 
-//function Title(props) { //propcidades / propriedades <3
-//   return (
-//    <h1>
-//      {props.children}
-//    </h1>
-//    )
-//}
-
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `;
+
+
 
 export default function Home() {
   return (
-   <BackgroundImage>
-      ashuashuahshua
-   </BackgroundImage>
+   <QuizBackground backgroundImage={db.bg}>
+     <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>    
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+     </QuizContainer>
+     <GitHubCorner projectUrl="https://github.com/omariosouto" />
+   </QuizBackground>
   ); 
 //  <Title>My page</Title>
 }
