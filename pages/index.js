62ad4711c4d4aa2b38eb36d4df8083e2091d9821
Vera Ueda <veraueda@hotmail.com>
2021-01-29 01:59:20 -0300

    Codigo da metade da segunda aula

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import styled from 'styled-components'
-import Head from 'next/head'
+/* eslint-disable spaced-comment */
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 //const BackgroundImage = styled.div`
 //  background-image: url(${db.bg});
@@ -25,15 +28,15 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const name = 'Vera';
   return (
-   <QuizBackground backgroundImage={db.bg}>
-     <Head>
-       <title>AluraQuiz - Modelo Base</title>
-     </Head>
-     <QuizContainer>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
+      <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>The legend of zelda</h1>
@@ -46,14 +49,24 @@ export default function Home() {
           <Widget.Header>
             <h1>Quizes da Galera</h1>
           </Widget.Header>
-          <Widget.Content>    
-            <p>lorem ipsum dolor sit amet...</p>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input placeholder="Entre com seu nome" />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Footer />
-     </QuizContainer>
-     <GitHubCorner projectUrl="https://github.com/omariosouto" />
-   </QuizBackground>
-  ); 
-//  <Title>My page</Title>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  );
+  //  <Title>My page</Title>
 }
---
+++ pages/quiz.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+export default function QuizPage() {
+  return (
+    <div>
+      Página de quiz
+    </div>
+  );
+}
